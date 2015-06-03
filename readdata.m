39bba26dfe52272f8f86f45e4e32a3dcd41ec369
RomeroBarata <romero.barata@gmail.com>
2015-06-04 03:16:18 +1000

    Function to read the MNIST data set
    Implemented the function to read the MNIST data set.
    Minor changes to the general data reading function.

--- readdata.m
+++ readdata.m
@@ -1,16 +1,17 @@
 function data = readdata(filename)
 
-if strcmp(filename, 'att_faces')
-    data = readattfaces;
-elseif strcmp(filename, 'mnistdigits')
-    % data = readmnistdigits; % Coding it now
-elseif strcmp(filename, 'binaryalphabet')
-    data = readbinaryalphabet;
-elseif strcmp(filename, 'coil20')
-    data = readcoil20;
-else
-    data = [];
-    display('Invalid file name!');
+switch filename
+    case 'att_faces'
+        data = readattfaces;
+    case 'mnistdigits'
+        data = readmnistdigits;
+    case 'binaryalphabet'
+        data = readbinaryalphabet;
+    case 'coil20'
+        data = readcoil20;
+    otherwise
+        data = [];
+        display('Invalid file name!')
 end
 
 end
